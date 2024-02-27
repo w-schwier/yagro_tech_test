c55f2822bdd4b405e8fdeb0268e6bd2f234c781b
Will Schwier <w.schwier@hotmail.com>
2024-02-27 08:00:26 +0000

    Refactor worker class to improve seperation of responsibility in functions

--- factory_simulator/worker.py
+++ factory_simulator/worker.py
@@ -10,22 +10,31 @@ class Worker:
         self.row = row
         self.held = []
 
+    def _can_place_product(self, belt: Belt) -> bool:
+        return Item.P in self.held and belt.slots[self.belt_position] is Item.EMPTY
+
+    def _can_pick_up_component(self, item: Item) -> bool:
+        return len(self.held) < self.NUMBER_OF_HANDS and item in Item.COMPONENT and item not in self.held
+
+    def _can_assemble(self) -> bool:
+        return Item.A in self.held and Item.B in self.held
+
     def _place_product(self, belt: Belt) -> bool:
-        if Item.P in self.held and belt.slots[self.belt_position] is Item.EMPTY:
+        if self._can_place_product(belt):
             belt.slots[self.belt_position] = self.held.pop(self.held.index(Item.P))
             return True
         return False
 
     def _pick_up_component(self, belt: Belt) -> bool:
         item = belt.slots[self.belt_position]
-        if len(self.held) < self.NUMBER_OF_HANDS and item in Item.COMPONENT and item not in self.held:
+        if self._can_pick_up_component(item):
             self.held.append(item)
             belt.slots[self.belt_position] = Item.EMPTY
             return True
         return False
 
     def _assemble(self) -> bool:
-        if Item.A in self.held and Item.B in self.held:
+        if self._can_assemble():
             self.held = [Item.P]
             return True
         return False
@@ -33,8 +42,9 @@ class Worker:
     def take_action(self, belt: Belt) -> bool:
         if self._place_product(belt):
             return True
-        if self._pick_up_component(belt):
+        elif self._pick_up_component(belt):
             return True
-        if self._assemble():
+        elif self._assemble():
             return True
-        return False
+        else:
+            return False
